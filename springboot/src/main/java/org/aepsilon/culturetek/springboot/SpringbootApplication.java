f05a546589b4601fb4aabd1baacf4f59db5e847b
boisseau <sboisseau@aepsilon.com>
2020-10-23 09:32:05 +0200

    add springboot sample implementation

---
+++ springboot/src/main/java/org/aepsilon/culturetek/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.aepsilon.culturetek.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
